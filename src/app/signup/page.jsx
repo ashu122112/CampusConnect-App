e9de66d9a8029bbf2831bc1baf9e4cc1baab26e2
Vivek300705 <135797974+Vivek300705@users.noreply.github.com>
2024-11-02 16:26:46 +0530

    first commit

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,97 @@
+"use client"
+import   {useState}  from 'react';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const Page = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-200 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="max-w-md mx-auto">
+            <h1 className="text-3xl flex justify-center text-slate-500 font-semibold">campussConnect</h1>
+
+            <div className="divide-y divide-gray-200">
+              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+
+                {/* Username Field */}
+                <div className="relative">
+                  <input
+                    autoComplete="off"
+                    id="username"
+                    name="username"
+                    type="text"
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
+                    placeholder="Username"
+                  />
+                  <label
+                    htmlFor="username"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                  >
+                    Username
+                  </label>
+                </div>
+
+                {/* Email Field */}
+                <div className="relative">
+                  <input
+                    autoComplete="off"
+                    id="email"
+                    name="email"
+                    type="text"
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
+                    placeholder="Email Address"
+                  />
+                  <label
+                    htmlFor="email"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                  >
+                    Email Address
+                  </label>
+                </div>
+
+                {/* Password Field with Eye Icon */}
+                <div className="relative">
+                  <input
+                    autoComplete="off"
+                    id="password"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
+                    placeholder="Password"
+                  />
+                  <label
+                    htmlFor="password"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                  >
+                    Password
+                  </label>
+                  <div
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer text-gray-500"
+                    onClick={togglePasswordVisibility}
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </div>
+                </div>
+
+                {/* Submit Button */}
+                <div className="relative">
+                  <button className="bg-blue-500 text-white rounded-md px-4 py-2 hover:text-blue-100">Submit</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page;
