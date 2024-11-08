9af123f09a8770b948d4e92fa1039116eda1ecfb
Vivek300705 <vivekkumarsulaniya@gmail.com>
2024-11-08 12:48:24 +0530

    done with axios

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,9 +23,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        {/* You can add other favicons for different sizes and formats if needed */}
-        {/* <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" /> */}
-        {/* <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" /> */}
       </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
